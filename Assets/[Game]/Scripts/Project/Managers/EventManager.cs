4721d8c5ceaae1fbdb131d42b630fd1e68321d0d
ladylovelacee <bgizembulut@gmail.com>
2021-01-01 16:03:53 +0300

    Initial commit

---
+++ Assets/[Game]/Scripts/Project/Managers/EventManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.Events;
+
+public static class EventManager
+{
+    #region Game State Events
+    public static UnityEvent OnGameStart = new UnityEvent();
+    public static UnityEvent OnGameOver = new UnityEvent();
+    #endregion
+
+    #region Level Events
+    public static UnityEvent OnLevelStart = new UnityEvent();
+    public static UnityEvent OnLevelFinish = new UnityEvent();
+    public static UnityEvent OnLevelFail = new UnityEvent();
+    #endregion
+
+    #region Data Events
+    public static PlayerDataEvent OnPlayerDataUpdated = new PlayerDataEvent();
+    #endregion
+}
+public class PlayerDataEvent : UnityEvent<PlayerData> { }
---
+++ Assets/[Game]/Scripts/Project/Managers/Managers.cs
@@ -0,0 +1,5 @@
+﻿
+public class Managers : Singleton<Managers>
+{
+    
+}
